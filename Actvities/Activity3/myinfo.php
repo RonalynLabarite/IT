a53e6689e9f2cbb4997b58da22eded6571ec67cb
Ronalyn p. Labarite <ronalynlabarite10@gmail.com>
2024-01-25 19:02:08 +0800

    First Commit of activity2

---
+++ Actvities/Activity3/myinfo.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <title>Activity 3</title>
+</head>
+<body>
+    <?php
+    echo "First Name: Ronalyn \n";
+    ?>
+    <br>
+    <?php
+    echo "Last Name: Labarite";
+    ?>
+    <br>
+    <?php
+    echo "Age: 20";
+    ?>
+    <br>
+    <?php
+    echo "Course: BSInfoTech Major in Programming";
+    ?>
+    <br>
+    <?php
+    echo "Address: Tubod, Silago Southern Leyte";
+    ?>
+    </form>
+</body>
+</html>            
